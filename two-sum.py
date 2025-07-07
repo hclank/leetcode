d589392921c011716f6dd137d3eb7f1770c19f5a
hclanka <152237163+chandudagoat@users.noreply.github.com>
2025-07-07 23:26:58 +0530

    finished

---
+++ two-sum.py
@@ -0,0 +1,10 @@
+def twoSum(nums: list[int], target: int) -> list[int]:
+    n = len(nums)
+    numsMap = {}
+
+    for i in range(n):
+        difference = target - nums[i]
+        if difference in numsMap:
+             print(f"{numsMap[difference]}, {i}")
+    
+
